71e879f23439795f14ec9593345e220edcdf77d2
Andrei Lastachkin <39839965+lastachkin@users.noreply.github.com>
2021-04-09 19:32:46 +0300

    db fix

--- TravelHelper/app/src/main/java/com/example/travelhelper/mvp/presenter/LoginPresenter.java
+++ TravelHelper/app/src/main/java/com/example/travelhelper/mvp/presenter/LoginPresenter.java
@@ -2,47 +2,39 @@ package com.example.travelhelper.mvp.presenter;
 
 import android.util.Log;
 
-import androidx.annotation.Nullable;
-
 import com.example.travelhelper.mvp.contract.LoginContract;
 import com.example.travelhelper.mvp.repository.Repository;
-import com.example.travelhelper.mvp.repository.model.User;
 import com.example.travelhelper.utils.Constants;
-import com.example.travelhelper.utils.Extensions;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.disposables.CompositeDisposable;
 
 
 public class LoginPresenter implements LoginContract.Presenter {
 
     private final LoginContract.View view;
     Repository repository;
+    CompositeDisposable mDisposable;
 
     public LoginPresenter(LoginContract.View view){
         this.view = view;
         repository = new Repository();
+        mDisposable = new CompositeDisposable();
     }
 
     @Override
     public void loginButtonClicked(String login, String password) {
         assert view != null;
-        repository.searchUser(login, password)
-                  .subscribeOn(Schedulers.io())
-                  .observeOn(AndroidSchedulers.mainThread())
-                  .subscribe(new DisposableSingleObserver<User>() {
-                      @Override
-                      public void onSuccess(User user) {
-                          view.onUserFound();
-                      }
-
-                      @Override
-                      public void onError(Throwable e) {
-                          Log.e(Constants.appLog, e.getMessage());
-                          view.onUserNotFound();
-                      }
-                  });
+        mDisposable.add(repository.searchUser(login, password)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(user -> {
+                    view.onUserFound();
+                }, throwable -> {
+                    Log.e(Constants.appLog, throwable.getMessage());
+                    view.onUserNotFound();
+                }));
     }
 
     @Override
